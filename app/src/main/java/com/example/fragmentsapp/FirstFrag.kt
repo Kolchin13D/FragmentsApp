c6d3c3c5d2797807b2578aed952a36faad6a3d29
Kolchin13D <kolchin13d@gmail.com>
2024-09-04 11:26:06 +0300

    testing

--- app/src/main/java/com/example/fragmentsapp/FirstFrag.kt
+++ app/src/main/java/com/example/fragmentsapp/FirstFrag.kt
@@ -1,10 +1,12 @@
- package com.example.fragmentsapp
+package com.example.fragmentsapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 class FirstFrag : Fragment() {
 
@@ -12,8 +14,39 @@ class FirstFrag : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Toast.makeText(context, "stage: onCreateView", Toast.LENGTH_SHORT).show()
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Toast.makeText(context, "stage: onAttach", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(context, "stage: onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(context, "stage: onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(context, "stage: onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(context, "stage: onDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(context, "stage: onResume", Toast.LENGTH_SHORT).show()
+    }
 }
